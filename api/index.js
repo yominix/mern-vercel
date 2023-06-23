759ca2543bc668691a06f7070eafb2b0dcbf412c
Yotin Sara <Yotin.Sara@brinks.com>
2023-06-23 21:40:48 +0700

    add date

--- api/index.js
+++ api/index.js
@@ -7,11 +7,11 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.json("Frist api to respon");
+    res.json("Frist api to respon "+Date.now());
 });
 
 app.get('/test', (req, res) => {
-    res.json("Test api to respon");
+    res.json("Test api to respon "+Date.now());
 });
 if (process.env.API_PORT) {
     app.listen(process.env.API_PORT);
